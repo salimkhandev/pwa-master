3f2d2aec6cda121f16b3cb3900419f4af503aecb
salimkhandev <salimt450s@gmail.com>
2025-02-24 01:29:04 +0500

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,6 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
